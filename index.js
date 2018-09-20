e5661c90bca8bab81c38705cedc561e45687f519
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-20 15:24:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var a = [];
+  for (var i = 0; i < musicians.length; i++) {
+  a.push(musicians[i] + " plays " + instruments[i])
+}
+return a;
+}
+
+function johnLennonFacts(facts) {
+  var b = 0;
+  var a = [];
+  while (b < facts.length) {  
+    a.push(facts[b] + "!!!");
+    b++;
+  }  
+  return a
+}
+
+
+
+  
+
+  
+
